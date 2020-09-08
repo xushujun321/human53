54eed2fa26654b6084c57511d403c3352b977bd3
xsj321 <1932806752@qq.com>
2020-09-08 08:09:18 +0800

    人力资源项目初始化

---
+++ src/settings.js
@@ -0,0 +1,19 @@
+module.exports = {
+  // 三个属性
+  // title (项目名称) ，
+  // fixedHeader固定头部），
+  // sidebarLogo（显示左侧菜单logo）
+  title: 'Vue Admin Template',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+}
---
+++ src/store/getters.js
@@ -0,0 +1,14 @@
+// const getters = {
+//   sidebar: state => state.app.sidebar,
+//   device: state => state.app.device,
+//   token: state => state.user.token,
+//   avatar: state => state.user.avatar,
+//   name: state => state.user.name
+// }
+// export default getters
+
+const getters = {
+  sidebar: state => state.app.sidebar,
+  device: state => state.app.device
+}
+export default getters
