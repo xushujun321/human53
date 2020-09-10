f027564122c2cf90d997ee867d2a1b0fac294ec2
xsj321 <1932806752@qq.com>
2020-09-10 16:57:23 +0800

    设置axios的baseURL与超出时间

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,90 +3,27 @@
 // import store from '@/store'
 // import { getToken } from '@/utils/auth'
 
-// // create an axios instance
-// const service = axios.create({
-//   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
-//   // withCredentials: true, // send cookies when cross-domain requests
-//   timeout: 5000 // 超时时间
-// })
-
-// // request interceptor   axios的请求拦截器
-// service.interceptors.request.use(
-//   config => {
-//     // do something before request is sent
-
-//     if (store.getters.token) {
-//       // let each request carry token
-//       // ['X-Token'] is a custom headers key
-//       // please modify it according to the actual situation
-//       config.headers['X-Token'] = getToken()
-//     }
-//     return config
-//   },
-//   error => {
-//     // do something with request error
-//     console.log(error) // for debug
-//     return Promise.reject(error)
-//   }
-// )
-
-// // response interceptor   响应拦截器
-// service.interceptors.response.use(
-//   /**
-//    * If you want to get http information such as headers or status
-//    * Please return  response => response
-//   */
-
-//   /**
-//    * Determine the request status by custom code
-//    * Here is just an example
-//    * You can also judge the status by HTTP Status Code
-//    */
-//   response => {
-//     const res = response.data
-
-//     // if the custom code is not 20000, it is judged as an error.
-//     if (res.code !== 20000) {
-//       Message({
-//         message: res.message || 'Error',
-//         type: 'error',
-//         duration: 5 * 1000
-//       })
-
-//       // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
-//       if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-//         // to re-login
-//         MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
-//           confirmButtonText: 'Re-Login',
-//           cancelButtonText: 'Cancel',
-//           type: 'warning'
-//         }).then(() => {
-//           store.dispatch('user/resetToken').then(() => {
-//             location.reload()
-//           })
-//         })
-//       }
-//       return Promise.reject(new Error(res.message || 'Error'))
-//     } else {
-//       return res
-//     }
-//   },
-//   error => {
-//     console.log('err' + error) // for debug
-//     Message({
-//       message: error.message,
-//       type: 'error',
-//       duration: 5 * 1000
-//     })
-//     return Promise.reject(error)
-//   }
-// )
-
-// export default service
-
 import axios from 'axios'
-const service = axios.create({}) // 创建新实例
+import { Message } from 'element-ui'
+
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API, // 设置请求的基础地址
+  timeout: 5000 // 定义超时时间
+}) // 创建新实例
 service.interceptors.request.use() // 请求拦截器
-service.interceptors.response.use() // 响应拦截器
+service.interceptors.response.use(response => {
+  //   接收数据
+  const { success, data, message } = response.data
+  if (success) {
+    // 如果success 说明执行是OK的 可以继续往下执行 可以直接将data返回
+    return data // 直接返回数据
+  } else {
+    Message.error(message) // 提示错误信息
+    return Promise.reject(new Error(message)) // 此时需要人为的reject当前的执行链 因为错了
+  }
+}, error => {
+  Message.error(error.message) // 提示错误信息
+  return Promise.reject(error)
+}) // 响应拦截器
 export default service // 导出新实例
 
