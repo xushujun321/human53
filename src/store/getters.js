b0ab3a117c37293445eccca24921107c60c0b00a
xsj321 <1932806752@qq.com>
2020-09-10 16:54:26 +0800

    将token暴露在全局，方便其他组件使用

--- src/store/getters.js
+++ src/store/getters.js
@@ -9,6 +9,8 @@
 
 const getters = {
   sidebar: state => state.app.sidebar,
-  device: state => state.app.device
+  device: state => state.app.device,
+  // 在根级的getters上，将state的user文件内的token值
+  token: state => state.user.token // 方便其他模块更好的访问token
 }
 export default getters
