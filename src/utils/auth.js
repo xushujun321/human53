aa9c639b30d86210cbd9519989748a401ac35db4
xsj321 <1932806752@qq.com>
2020-09-10 16:56:33 +0800

    设置token的key值，并设置好对token的获取、设置和删除的方法

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,15 +1,15 @@
 import Cookies from 'js-cookie'
 
-const TokenKey = 'vue_admin_template_token'
-
+const TokenKey = 'hrsaas53-token' // 存储token的key值
+// 获取token值的方法
 export function getToken() {
   return Cookies.get(TokenKey)
 }
-
+// 设置token值的方法
 export function setToken(token) {
   return Cookies.set(TokenKey, token)
 }
-
+// 删除token的方法
 export function removeToken() {
   return Cookies.remove(TokenKey)
 }
