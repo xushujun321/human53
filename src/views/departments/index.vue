1caf0ba73800bb89d973c31d74009de88c921d48
xsj321 <1932806752@qq.com>
2020-09-11 17:52:01 +0800

    删除多余的页面与新建多余的新页面

---
+++ src/views/departments/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>组织架构</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/employees/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>员工</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
