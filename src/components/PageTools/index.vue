97c6b1034278f3cd8acf3c97d49839afaeebd64c
xsj321 <1932806752@qq.com>
2020-09-15 16:07:11 +0800

    通用工具栏左右布局的封装

---
+++ src/components/PageTools/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <!-- 通用工具栏结构---左右布局，一分为二 -->
+  <el-card class="page-tools">
+    <el-row type="flex" justify="space-between" align="middle">
+      <el-col v-if="showBefore" class="before">
+        <!-- 左侧的坑 -->
+        <i class="el-icon-info" />
+        <slot name="before" />
+      </el-col>
+      <el-col>
+        <el-row type="flex" justify="end">
+          <!-- 右边的坑 -->
+          <slot name="after" />
+        </el-row>
+      </el-col>
+    </el-row>
+  </el-card>
+
+</template>
+
+<script>
+export default {
+  props: {
+    showBefore: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang='scss'>
+ .page-tools {
+    margin: 10px 0;
+    .before {
+      line-height: 34px;
+    i {
+      margin-right: 5px;
+      color: #409eff;
+    }
+    display: inline-block;
+    padding: 0px 10px;
+    border-radius: 3px;
+    border: 1px solid rgba(145, 213, 255, 1);
+    background: rgba(230, 247, 255, 1);
+  }
+ }
+</style>
