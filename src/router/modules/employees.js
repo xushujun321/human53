60d138ef69d10322edacacaae604614deec2ae88
xsj321 <1932806752@qq.com>
2020-09-17 20:28:19 +0800

    关于员工详情与打印的路由设置

--- src/router/modules/employees.js
+++ src/router/modules/employees.js
@@ -13,5 +13,24 @@ export default {
       title: '员工管理', // 标记当前路由规则的中文名称 后续在做左侧菜单时 使用
       icon: 'people'
     }
+  },
+  {
+    path: 'detail/:id', // 二级默认路由
+    component: () => import('@/views/employees/detail'), // 按需加载
+    hidden: true,
+    meta: {
+      title: '员工详情', // 标记当前路由规则的中文名称 后续在做左侧菜单时 使用
+      icon: 'people'
+    }
+  },
+  {
+    path: 'print/:id', // 二级默认路由
+    component: () => import('@/views/employees/print'), // 按需加载
+    hidden: true,
+    meta: {
+      title: '打印', // 标记当前路由规则的中文名称 后续在做左侧菜单时 使用
+      icon: 'people'
+    }
   }]
 }
+
