43988e8ca975fc28767e076c90aa892ac7259d7e
xsj321 <1932806752@qq.com>
2020-09-17 20:27:21 +0800

    关于导入文件组件与上传图片组件的全局注册

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,16 @@
 // 公共组件的全局注册
 
+// 左右布局的组件
 import pageTools from './PageTools'
+// 导入Excel组件
+import UploadExcel from './UploadExcel'
+// 引进图片全局
+import ImageUpload from './ImageUpload'
 
 export default {
   install(Vue) {
-    Vue.component('pageTools', pageTools)
+    Vue.component('pageTools', pageTools) // 注册工具栏组件
+    Vue.component('UploadExcel', UploadExcel)// 注册导入excel组件
+    Vue.component('ImageUpload', ImageUpload)// 图片上传组件
   }
 }
