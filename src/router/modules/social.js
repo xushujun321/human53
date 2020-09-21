371da3b5001c0de32f98e2c83a9a88a06f6ff842
xsj321 <1932806752@qq.com>
2020-09-21 14:32:59 +0800

    关于社保的页面与js文件

--- src/router/modules/social.js
+++ src/router/modules/social.js
@@ -1,16 +1,50 @@
+
 import Layout from '@/layout'
 
 export default {
+  path: '/social_securitys',
   component: Layout,
-  path: '/social',
-  name: 'social',
+  name: 'social_securitys',
+  children: [
+    {
+      path: '',
+      component: () => import('@/views/social'),
+      name: 'social_securitys',
+      meta: {
+        title: '社保',
+        icon: 'table'
 
-  children: [{
-    path: '',
-    component: () => import('@/views/social'),
-    meta: {
-      title: '工资管理',
-      icon: 'table'
+      }
+    },
+    // 报表
+    {
+      path: 'detail/:id',
+      hidden: true,
+      component: () => import('@/views/social/detail'),
+      name: 'socialDetail',
+      meta: {
+        title: '社保'
+      }
+    },
+    // 历史归档
+    {
+      path: 'historicalArchiving',
+      hidden: true,
+      component: () => import('@/views/social/historical'),
+      name: 'socialHistorical',
+      meta: {
+        title: '历史归档'
+      }
+    },
+    // 月报表
+    {
+      path: 'monthStatement',
+      component: () => import('@/views/social/month'),
+      name: 'socialMonthStatement',
+      hidden: true,
+      meta: {
+        title: '当月报表'
+      }
     }
-  }]
+  ]
 }
