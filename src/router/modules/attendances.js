99fe0465d73c9cb01773fffb59c066a85e0b4553
xsj321 <1932806752@qq.com>
2020-09-21 16:23:59 +0800

    考勤页面与js

--- src/router/modules/attendances.js
+++ src/router/modules/attendances.js
@@ -1,18 +1,37 @@
-// 引进布局组件
+
 import Layout from '@/layout'
 
-export default {
-  component: Layout, // 当前组件
-  path: '/attendances', // 路由地址
-  name: 'attendances', // 路由名称
-
-  children: [{
-    path: '', // 二级默认路由
-    component: () => import('@/views/attendances'), // 按需加载
-    // 为什么meta要在二级默认路由写 因为 当 访问地址是 /approvals的时候 实际上访问的是二级默认路由的路由信息
-    meta: {
-      title: '考勤管理', // 用作左边侧边栏
-      icon: 'skill'
+const attendRouter = {
+  path: '/attendances',
+  component: Layout,
+  name: 'attendances',
+  children: [
+    {
+      path: '',
+      component: () => import('@/views/attendances'),
+      name: 'attendances',
+      meta: {
+        title: '考勤',
+        icon: 'excel' }
+    },
+    {
+      path: 'archiving',
+      component: () => import('@/views/attendances/historical'),
+      name: 'archiving',
+      hidden: true,
+      meta: {
+        title: '归档'
+      }
+    },
+    {
+      path: 'report/:month',
+      component: () => import('@/views/attendances/report'),
+      name: 'reports',
+      hidden: true,
+      meta: {
+        title: '报表'
+      }
     }
-  }]
+  ]
 }
+export default attendRouter
