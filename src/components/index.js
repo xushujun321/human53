e61f178ada7ee6611fcb4e49a4228ad39e2bc514
xsj321 <1932806752@qq.com>
2020-09-24 17:57:51 +0800

    动态主题与全屏组件的全局注册

--- src/components/index.js
+++ src/components/index.js
@@ -6,6 +6,10 @@ import pageTools from './PageTools'
 import UploadExcel from './UploadExcel'
 // 引进图片全局
 import ImageUpload from './ImageUpload'
+// 全屏插件的注册
+import ScreenFull from './ScreenFull'
+// 动态组件的注册
+import ThemePicker from './ThemePicker'
 // 上传图片的练习
 // import ImageUpload2 from './ImageUpload/index2.vue'
 
@@ -14,6 +18,8 @@ export default {
     Vue.component('pageTools', pageTools) // 注册工具栏组件
     Vue.component('UploadExcel', UploadExcel)// 注册导入excel组件
     Vue.component('ImageUpload', ImageUpload)// 图片上传组件
+    Vue.component('ScreenFull', ScreenFull)// 全屏插件
+    Vue.component('ThemePicker', ThemePicker)// 动态主题插件
     // Vue.component('ImageUpload2', ImageUpload2)// 图片上传组件
   }
 }
