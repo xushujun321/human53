569960615cd9b0e54a1ddd1db69f66aeba3cb5c0
xsj321 <1932806752@qq.com>
2020-09-24 17:55:27 +0800

    首页的日历组件的封装

---
+++ src/views/dashboard/components/work-calendar.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <!-- 放置年和月 -->
+    <el-row type="flex" justify="end">
+      <el-select v-model="currentYear" size="small" style="width: 120px" @change="changeDate">
+        <el-option v-for="item in yearList" :key="item" :label="item" :value="item" />
+      </el-select>
+      <el-select v-model="currentMonth" size="small" style="width: 120px; margin-left: 10px" @change="changeDate">
+        <el-option v-for="item in 12" :key="item" :label="item" :value="item">{{ item }}</el-option>
+      </el-select>
+    </el-row>
+    <el-calendar v-model="currentDate">
+      <!-- 插槽 -->
+      <template slot="dateCell" slot-scope="{date, data }">
+        <div class="date-content">
+          <span class="text"> {{ data.day | getDay }}</span>
+          <span v-if="isWeek(date)" class="rest">休</span>
+        </div>
+
+      </template>
+    </el-calendar>
+  </div>
+</template>
+
+<script>
+export default {
+  filters: {
+    // 2020-09-02
+    getDay(value) {
+      const day = value.split('-')[2]
+      return day.startsWith('0') ? day.substr(1) : day
+    }
+  },
+  props: {
+    setDate: {
+      type: Date,
+      default: () => new Date()
+    }
+  },
+  data() {
+    return {
+    // 年份
+      currentYear: null,
+      // 月份
+      currentMonth: null,
+      // 年份数组
+      yearList: [],
+      // 当前时间拼接
+      currentDate: null
+    }
+  },
+  created() {
+    this.currentYear = this.setDate.getFullYear()
+    this.currentMonth = this.setDate.getMonth() + 1
+    this.yearList = Array.from(Array(11), (value, index) => this.currentYear + index - 5)
+    this.changeDate()
+  },
+  methods: {
+    changeDate() {
+      this.currentDate = new Date(`${this.currentYear}-${this.currentMonth}-1`)
+    },
+    isWeek(value) {
+      return value.getDay() === 6 || value.getDay() === 0
+    }
+  }
+}
+</script>
+
+  <style scoped>
+    /deep/ .el-calendar-day {
+    height:  auto;
+    }
+    /deep/ .el-calendar-table__row td, /deep/ .el-calendar-table tr td:first-child,  /deep/ .el-calendar-table__row td.prev{
+    border:none;
+    }
+    .date-content {
+    height: 40px;
+    text-align: center;
+    line-height: 40px;
+    font-size: 14px;
+    }
+    .date-content .rest {
+    color: #fff;
+    border-radius: 50%;
+    background: rgb(250, 124, 77);
+    width: 20px;
+    height: 20px;
+    line-height: 20px;
+    display: inline-block;
+    font-size: 12px;
+    margin-left: 10px;
+    }
+    .date-content .text{
+    width: 20px;
+    height: 20px;
+    line-height: 20px;
+    display: inline-block;
+
+    }
+    /deep/ .el-calendar-table td.is-selected .text{
+    background: #409eff;
+    color: #fff;
+    border-radius: 50%;
+    }
+  </style>
+</template>
