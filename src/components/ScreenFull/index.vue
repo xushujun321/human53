1406e59e6ceefb8a3361b2e55d0ddf17e993d855
xsj321 <1932806752@qq.com>
2020-09-24 17:57:00 +0800

    全屏组件的封装

---
+++ src/components/ScreenFull/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <svg-icon icon-class="fullscreen" style="color:#fff; width:20px; height:20px" @click="changeScreen" />
+  </div>
+</template>
+
+<script>
+import screenfull from 'screenfull'
+export default {
+  methods: {
+    changeScreen() {
+      if (!screenfull.isEnabled) {
+        this.$message.warning('此时不能全屏')
+        return
+      }
+      screenfull.toggle()// 不断进行全屏方法
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
